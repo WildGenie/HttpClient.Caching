2c23bbaca3d9382c608bf2fc7a756a335a74ac7a
Damian Hickey <dhickey@gmail.com>
2016-09-19 13:57:29 +0200

    Import CacheCow Client and update some package dependencies.

---
+++ src/CacheCow.Client.FileCacheStore/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("CacheCow.Client.FileCacheStore")]
+[assembly: AssemblyDescription("File storage for HTTP caching in CacheCow library")]
+
+[assembly: InternalsVisibleTo("CacheCow.Client.FileCacheStore.Tests")]
