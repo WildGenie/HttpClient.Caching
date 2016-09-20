c7e21e8c109b97213f2b2d8ec70af7b880b91443
Damian Hickey <dhickey@gmail.com>
2016-09-20 12:26:03 +0200

    Start of new projects.

---
+++ src/HttpClient.Caching.Tests/CacheTests.cs
@@ -0,0 +1,37 @@
+﻿namespace HttpClient.Caching.Tests
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Microsoft.Owin.Builder;
+    using Owin;
+    using Xunit;
+
+    public class CacheTests
+    {
+        [Fact]
+        public async Task Blah()
+        {
+            var app = new AppBuilder();
+            app.Run(ctx =>
+            {
+                ctx.Response.StatusCode = 404;
+                ctx.Response.ReasonPhrase = "Not Found";
+                return Task.CompletedTask;
+            });
+            var appFunc = app.Build();
+            var handler = new OwinHttpMessageHandler(appFunc)
+            {
+                AllowAutoRedirect = true,
+                UseCookies = true
+            };
+
+            var client = new HttpClient(handler)
+            {
+                BaseAddress = new Uri("http://example.com")
+            };
+
+            await client.GetAsync("");
+        }
+    }
+}
---
+++ src/HttpClient.Caching/ICacheStore.cs
@@ -0,0 +1,17 @@
+﻿namespace HttpClient.Caching
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public interface ICacheStore : IDisposable
+    {
+        Task<bool> TryGetValue(CacheKey key, out HttpResponseMessage response);
+
+        Task AddOrUpdate(CacheKey key, HttpResponseMessage response);
+
+        Task<bool> TryRemove(CacheKey key);
+
+        Task Clear();
+    }
+}
