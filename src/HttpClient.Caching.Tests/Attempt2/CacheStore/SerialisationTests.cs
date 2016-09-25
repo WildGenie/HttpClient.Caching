7d6f959d7330c481421602c65106fcefb017a85c
Damian Hickey <dhickey@gmail.com>
2016-09-25 23:36:45 +0200

    merg

---
+++ src/HttpClient.Caching.Tests/Attempt2/CacheStore/SerialisationTests.cs
@@ -0,0 +1,107 @@
+﻿namespace HttpClient.Caching.Attempt2.CacheStore
+{
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using HttpClient.Caching.Helper;
+    using Shouldly;
+    using Xunit;
+
+    public class SerialisationTests
+    {
+        [Fact]
+        public async Task Request_Deserialize_Serialize()
+        {
+            var stream =
+                Assembly.GetExecutingAssembly().GetManifestResourceStream("HttpClient.Caching.Data.Request.dat");
+            var request = await MessageContentSerializer.DeserializeToRequest(stream);
+
+            var memoryStream = new MemoryStream();
+            await MessageContentSerializer.Serialize(request, memoryStream);
+
+            memoryStream.Position = 0;
+            var request2 = await MessageContentSerializer.DeserializeToRequest(memoryStream);
+            var result = DeepComparer.Compare(request, request2);
+
+            result.Count().ShouldBe(0, string.Join("\r\n", result));
+        }
+
+        [Fact]
+        public async Task Request_Deserialize_Serialize_File()
+        {
+            var stream =
+                Assembly.GetExecutingAssembly().GetManifestResourceStream("HttpClient.Caching.Data.Request.dat");
+            var request = await MessageContentSerializer.DeserializeToRequest(stream);
+
+            using(var fileStream = new FileStream("request.tmp", FileMode.Create))
+            {
+                await MessageContentSerializer.Serialize(request, fileStream);
+
+                fileStream.Position = 0;
+                var request2 = await MessageContentSerializer.DeserializeToRequest(fileStream);
+                var result = DeepComparer.Compare(request, request2);
+
+                result.Count().ShouldBe(0, string.Join("\r\n", result));
+            }
+        }
+
+        [Fact]
+        public async Task Response_Deserialize_Serialize1()
+        {
+            var stream =
+                    Assembly.GetExecutingAssembly().GetManifestResourceStream("HttpClient.Caching.Data.Response.dat");
+
+            var memoryStream = new MemoryStream();
+
+            while (true)
+            {
+                var response = await MessageContentSerializer.DeserializeToResponse(stream);
+                await MessageContentSerializer.Serialize(response, Stream.Null);
+                memoryStream.Position = 0;
+            }
+
+            /*memoryStream.Position = 0;
+            var response2 = await MessageContentSerializer.DeserializeToResponse(memoryStream);
+            var result = DeepComparer.Compare(response, response2);
+
+            result.Count().ShouldBe(0, string.Join("\r\n", result));*/
+        }
+
+        [Fact]
+        public async Task Response_Deserialize_Serialize()
+        {
+            var stream =
+                Assembly.GetExecutingAssembly().GetManifestResourceStream("HttpClient.Caching.Data.Response.dat");
+            var response = await MessageContentSerializer.DeserializeToResponse(stream);
+
+            var memoryStream = new MemoryStream();
+            await MessageContentSerializer.Serialize(response, memoryStream);
+
+            memoryStream.Position = 0;
+            var response2 = await MessageContentSerializer.DeserializeToResponse(memoryStream);
+            var result = DeepComparer.Compare(response, response2);
+
+            result.Count().ShouldBe(0, string.Join("\r\n", result));
+        }
+
+        [Fact]
+        public async Task Response_Deserialize_Serialize_File()
+        {
+            var stream =
+                Assembly.GetExecutingAssembly().GetManifestResourceStream("HttpClient.Caching.Data.Response.dat");
+            var response = await MessageContentSerializer.DeserializeToResponse(stream);
+
+            using(var fileStream = new FileStream("response.tmp", FileMode.Create))
+            {
+                await MessageContentSerializer.Serialize(response, fileStream);
+
+                fileStream.Position = 0;
+                var response2 = await MessageContentSerializer.DeserializeToResponse(fileStream);
+                var result = DeepComparer.Compare(response, response2);
+
+                result.Count().ShouldBe(0, string.Join("\r\n", result));
+            }
+        }
+    }
+}
---
+++ src/HttpClient.Caching/Attempt2/CacheStore/MessageContentSerializer.cs
@@ -0,0 +1,51 @@
+﻿namespace HttpClient.Caching.Attempt2.CacheStore
+{
+    using System.IO;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+
+    public static class MessageContentSerializer
+    {
+        public static async Task Serialize(HttpResponseMessage response, Stream stream)
+        {
+            if(response.Content != null)
+            {
+                await response.Content.LoadIntoBufferAsync();
+            }
+            var httpMessageContent = new HttpMessageContent(response);
+            var buffer = await httpMessageContent.ReadAsByteArrayAsync().ConfigureAwait(false);
+            await stream.WriteAsync(buffer, 0, buffer.Length);
+        }
+
+        public static async Task Serialize(HttpRequestMessage request, Stream stream)
+        {
+            if(request.Content != null)
+            {
+                await request.Content.LoadIntoBufferAsync();
+            }
+            var httpMessageContent = new HttpMessageContent(request);
+            var buffer = await httpMessageContent.ReadAsByteArrayAsync();
+            await stream.WriteAsync(buffer, 0, buffer.Length);
+        }
+
+        public static Task<HttpResponseMessage> DeserializeToResponse(Stream stream)
+        {
+            var response = new HttpResponseMessage
+            {
+                Content = new StreamContent(stream)
+            };
+            response.Content.Headers.Add("Content-Type", "application/http;msgtype=response");
+            return response.Content.ReadAsHttpResponseMessageAsync();
+        }
+
+        public static Task<HttpRequestMessage> DeserializeToRequest(Stream stream)
+        {
+            var request = new HttpRequestMessage
+            {
+                Content = new StreamContent(stream)
+            };
+            request.Content.Headers.Add("Content-Type", "application/http;msgtype=request");
+            return request.Content.ReadAsHttpRequestMessageAsync();
+        }
+    }
+}
